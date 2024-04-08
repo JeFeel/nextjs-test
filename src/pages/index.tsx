ffba2849e458ea1aa6466b2194b7194fa8514033
JeFeel <jaephil94@gmail.com>
2024-04-08 15:44:52 +0900

    center fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import type { ReactElement } from "react";
 import Layout from "../components/layout";
-// import NestedLayout from "../components/nested-layout";
 import type { NextPageWithLayout } from "./_app";
+import style from "styled-components";
+
+const MainPage = style.div`
+  padding : 10pt;
+  text-align : center;
+`;
 
 const Page: NextPageWithLayout = () => {
-  return <p>hello world</p>;
+  return <MainPage>hello world</MainPage>;
 };
 
 Page.getLayout = function getLayout(page: ReactElement) {
